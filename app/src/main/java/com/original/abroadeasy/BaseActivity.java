d8622b681aeea507289172eba0dca83c5e1990dc
leo.yang <leo.yang@gigasetdigital.com>
2015-11-23 18:54:29 +0800

    rm original file in the external side

--- app/src/main/java/com/original/abroadeasy/BaseActivity.java
+++
@@ -1,9 +0,0 @@
-package com.original.abroadeasy;
-
-import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by zengjinlong on 15-10-28.
- */
-public class BaseActivity extends AppCompatActivity {
-}
--- app/src/main/java/com/original/abroadeasy/DetailActivity.java
+++
@@ -1,7 +0,0 @@
-package com.original.abroadeasy;
-
-/**
- * Created by zengjinlong on 15-11-2.
- */
-public class DetailActivity {
-}
