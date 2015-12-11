cf111927ebb32ec82436d4798fa7583753770af1
lyang850108 <lyang850108@163.com>
2015-12-11 16:39:38 +0800

    Del the setting menu in actionbar
    Del the setting menu in the menu_main.xml

--- app/src/main/java/com/original/abroadeasy/ui/MainActivity.java
+++ app/src/main/java/com/original/abroadeasy/ui/MainActivity.java
@@ -247,21 +247,6 @@ public class MainActivity extends BaseActivity implements ActionBarController.Ac
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     /**
      * If the search term is empty and the user closes the soft keyboard, close the search UI.
      */
@@ -320,4 +305,8 @@ public class MainActivity extends BaseActivity implements ActionBarController.Ac
         }
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
 }
